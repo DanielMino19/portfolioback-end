c9625651137b0e4721ffb5604a30a511a96fb4a6
DanielMino19 <danielmino926@gmail.com>
2023-04-24 13:02:10 -0300

    cors fix

--- src/main/java/com/clporfoliobackend/democl/security/CorsConfig.java
+++ src/main/java/com/clporfoliobackend/democl/security/CorsConfig.java
@@ -7,10 +7,11 @@ package com.clporfoliobackend.democl.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-
+@EnableWebMvc
 @Configuration
 public class CorsConfig {
     @Bean
