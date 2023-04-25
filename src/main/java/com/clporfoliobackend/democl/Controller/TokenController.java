946bf05db61d85adb50bdaca82e4b08db1db9044
DanielMino19 <danielmino926@gmail.com>
2023-04-25 06:02:18 -0300

    cors config fixed

--- src/main/java/com/clporfoliobackend/democl/Controller/TokenController.java
+++ src/main/java/com/clporfoliobackend/democl/Controller/TokenController.java
@@ -9,6 +9,7 @@ import com.clporfoliobackend.democl.security.TokenUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class TokenController {
 private TokenUtils myTokenUtils;
 
     @PostMapping("/verify")
+    @PreAuthorize("permitAll()")
     public ResponseEntity<?> verifyToken(@RequestBody String token) {
         if (myTokenUtils.validateToken(token)) {
             return ResponseEntity.ok().build();
